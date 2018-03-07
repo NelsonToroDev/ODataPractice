75a564bae8c512b18f7bfffa91d0059f51a88421
nekero8 <nekero8@gmail.com>
2018-03-06 17:08:50 -0800

    Added WCF DataService to query Orders

---
+++ OptimizeData/OrdersDataModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using OptimizeData.DataModel;
+
+namespace OptimizeData
+{
+    public class OrdersDataModel
+    {
+        public OrdersDataModel()
+        {
+            var processProjection = from p in Process.GetProcesses()
+                                    select new OrderModel()
+                                    {
+                                        Id = p.Id,
+                                        Name = p.ProcessName
+                                    };
+            Orders = processProjection.AsQueryable();
+        }
+
+        public IQueryable<OrderModel> Orders { get; set; }
+    }
+}
---
+++ OptimizePOC/OrderModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Services.Common;
+
+namespace OptimizePOC
+{
+    [DataServiceKey("Name")]
+    public class OrderModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
