4519db0f66a6c8e3bf3ff856f8de11d88c8aa004
nekero8 <nekero8@gmail.com>
2018-03-09 18:47:21 -0800

    Failed to host DataService using selft-hosting in console

--- OptimizePOC/MainAppLoader.cs
+++ OptimizePOC/MainAppLoader.cs
@@ -5,6 +5,7 @@ using log4net.Core;
 using log4net.Repository.Hierarchy;
 using Spring.Context;
 using Spring.Context.Support;
+using System.Data.Services;
 
 namespace OptimizePOC
 {
@@ -30,9 +31,10 @@ namespace OptimizePOC
         public void LoadAndRunApp()
         {
             ConfigureLogger();
+
             try
             {
-                this.context = ContextRegistry.GetContext();
+                //this.context = ContextRegistry.GetContext();
             }
             catch (Exception e)
             {
@@ -40,6 +42,33 @@ namespace OptimizePOC
                 Logger.Fatal("Rethrowing to the OS the exception raised");
                 throw;
             }
+
+            HostService();
+        }
+
+        public void HostService()
+        {
+            Type serviceType = typeof(OptimizeService);
+            Uri baseAddress = new Uri("http://localhost:6000/");
+            Uri[] baseAddresses = new Uri[] { baseAddress };
+
+            // Create a new hosting instance for the Northwind 
+            // data service at the specified address. 
+            DataServiceHost host = new DataServiceHost(
+               serviceType,
+               baseAddresses);
+            host.Open();
+
+            // Keep the data service host open while the console is open. 
+            Console.WriteLine(
+              "Navigate to the following URI to see the service.");
+            Console.WriteLine(baseAddress);
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadKey();
+
+            // Close the host. 
+            host.Close();
         }
 
         /// <summary>
--- OptimizePOC/OptimizeService.svc.cs
+++ OptimizePOC/OptimizeService.svc.cs
@@ -10,21 +10,21 @@ namespace OptimizePOC
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "OptimizeService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select OptimizeService.svc or OptimizeService.svc.cs at the Solution Explorer and start debugging.
     
-    public class OptimizeService : DataService<OptimizeDataSource>
+    public class OptimizeService : DataService<OptimizeDataSource> 
     {
-        public OptimizeService()
-        {
-            Configuration cfg = new Configuration();
-            cfg.Configure();
+        //public OptimizeService()
+        //{
+        //    //Configuration cfg = new Configuration();
+        //    //cfg.Configure();
 
-            // Add class mappings attributes to configuration object
-            //cfg.AddInputStream(HbmSerializer.Default.Serialize(typeof(Models.Order)));
-            ISessionFactory sessionFactory = cfg.BuildSessionFactory();
-        }
+        //    // Add class mappings attributes to configuration object
+        //    //cfg.AddInputStream(HbmSerializer.Default.Serialize(typeof(Models.Order)));
+        //    //ISessionFactory sessionFactory = cfg.BuildSessionFactory();
+        //}
 
         public static void InitializeService(DataServiceConfiguration config)
         {
-            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Orders", EntitySetRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
         }
     }
